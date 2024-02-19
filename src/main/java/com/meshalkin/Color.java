6be977d77a28b2493c90aec2dc8919d4b9f455d8
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-02-19 16:21:33 +0300

    fix: Удалил устаревший класс

--- src/main/java/com/meshalkin/Color.java
+++
@@ -1,9 +0,0 @@
-package com.meshalkin;
-
-public class Color {
-    public static final String RED = "\u001B[31m"; // ANSI код красного цвета
-    public static final String GREEN = "\u001B[32m"; // ANSI код зеленого цвета
-    public static final String YELLOW = "\u001B[33m"; // ANSI код желтого цвета
-    public static final String RESET = "\u001B[0m"; // сброс настроек
-
-}
