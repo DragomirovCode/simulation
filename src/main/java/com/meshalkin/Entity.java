0a2adda663cea23bccbc7f3101f99e9dc0dc7dc4
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-02-19 16:30:24 +0300

    feat: Убрал ненужные отступы

--- src/main/java/com/meshalkin/Entity.java
+++ src/main/java/com/meshalkin/Entity.java
@@ -2,12 +2,8 @@ package com.meshalkin;
 
 abstract public class Entity {
     public Coordinates coordinates;
-
-
-
     public Entity(Coordinates coordinates) {
         this.coordinates = coordinates;
     }
-
     public abstract String getSymbol(); // абстрактный метод для получения символа сущности
 }
