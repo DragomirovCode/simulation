96c924adfcf95cb157c6e79bce57bc1984283325
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-02-19 20:57:55 +0300

    fix: Изменил метод "generation"

--- src/main/java/com/meshalkin/GenerationCoordinates.java
+++ src/main/java/com/meshalkin/GenerationCoordinates.java
@@ -1,10 +1,9 @@
 package com.meshalkin;
 
-import com.meshalkin.entities.Predator;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class GenerationCoordinates {
 
@@ -12,22 +11,35 @@ public class GenerationCoordinates {
 
     private Random random = new Random();
 
-    private File[] files = File.values();
-
     public GenerationCoordinates(List<Creature> creatures) {
         this.creatures = creatures;
     }
+
 
     public Coordinates generation(){
         // Выбираем случайное существо
         int index = random.nextInt(creatures.size());
         Creature creature = creatures.get(index);
 
-        // Генерируем новые координаты
-        File x = files[random.nextInt(files.length)];
-        int y = random.nextInt(8) + 1; // добавляем 1, чтобы получить числа от 1 до 8
+        // Получаем доступные ходы для данного существа
+        Set<CoordinatesShift> availableMoves = creature.getEntityMoves();
+
+        // Выбираем случайный доступный ход
+        CoordinatesShift randomMove = getRandomMove(availableMoves);
+
+        // Получаем текущие координаты существа
+        Coordinates currentCoordinates = creature.getCoordinates();
+
+        // Получаем новые координаты путем смещения
+        File currentFile = currentCoordinates.x;
+        File newFile = File.values()[(currentFile.ordinal() + randomMove.x) % File.values().length];
+        int newY = currentCoordinates.y + randomMove.y;
 
         // Передаем новые координаты существу
-        return new Coordinates(x,y);
+        return new Coordinates(newFile, newY);
+    }
+    private CoordinatesShift getRandomMove(Set<CoordinatesShift> availableMoves) {
+        List<CoordinatesShift> movesList = new ArrayList<>(availableMoves);
+        return movesList.get(random.nextInt(movesList.size()));
     }
 }
