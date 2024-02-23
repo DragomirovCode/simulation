2631f06f45ffd507973bf0a10995cdb826c7b2e5
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-02-23 21:30:34 +0300

    feat: Тестируем существующий код

--- src/main/java/com/meshalkin/Main.java
+++ src/main/java/com/meshalkin/Main.java
@@ -1,11 +1,25 @@
 package com.meshalkin;
 
+import com.meshalkin.entities.Herbivore;
+import com.meshalkin.entities.Predator;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Mapping mapping = new Mapping();
-        mapping.setupPiecesPositions();
 
-        Simulation simulation = new Simulation();
-        simulation.render(mapping);
+        Coordinates start = new Coordinates(File.A, 1);
+        Coordinates end = new Coordinates(File.G, 2);
+
+        Creature creature = new Predator(start);
+
+        boolean result = creature.searchForAHerbivore(mapping, start, end);
+
+        if (result) {
+            System.out.println("Травоядное существо найдено на позиции " + end);
+        } else {
+            System.out.println("Травоядное существо не найдено на позиции " + end);
+        }
     }
 }
