3289a74bfb7c1a7a83ee620e260d4fd4335f4c5b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-02-26 14:30:14 +0300

    feat: Реализовал метод интерфейса для поиска травоядного

--- src/main/java/com/meshalkin/entities/Predator.java
+++ src/main/java/com/meshalkin/entities/Predator.java
@@ -2,18 +2,16 @@ package com.meshalkin.entities;
 
 import com.meshalkin.*;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class Predator extends Creature {
+public class Predator extends Creature implements TargetChooser {
 
     public Predator(Coordinates coordinates) {
         super(coordinates);
     }
 
     @Override
-    protected Set<CoordinatesShift> getEntityMoves() {
+    public Set<CoordinatesShift> getEntityMoves() {
         return new HashSet<>(Arrays.asList(
                 new CoordinatesShift(1,0),
                 new CoordinatesShift(-1,0),
@@ -24,5 +22,38 @@ public class Predator extends Creature {
     @Override
     public String getSymbol() {
         return "\uD83E\uDD8A";
+    }
+
+    // найти ближайшее существо и вернуть его координаты
+    @Override
+    public Coordinates chooseTarget(Creature creature, Mapping mapping) {
+        Coordinates predatorPosition = creature.getCoordinates();
+        Set<Coordinates> herbivorePositions = new HashSet<>();
+        // Находим координаты всех травоядных существ на карте
+        for(Coordinates coordinates: mapping.entities.keySet() ){
+            Entity entity = mapping.getEntity(coordinates);
+            if(entity instanceof Herbivore){
+                herbivorePositions.add(coordinates);
+            }
+        }
+        // Используем обход в ширину для поиска ближайшего травоядного
+        Queue<Coordinates> queue = new LinkedList<>();
+        Set<Coordinates> visited = new HashSet<>();
+        queue.add(predatorPosition);
+        visited.add(predatorPosition);
+        while (!queue.isEmpty()){
+            Coordinates current = queue.remove();
+            if(herbivorePositions.contains(current)){
+                return current;
+            }
+            for(CoordinatesShift shift: creature.getEntityMoves()){
+                Coordinates next = current.shift(shift);
+                if(next.canShift(shift) && !visited.contains(next) && mapping.isSquareEmpty(next)){
+                    queue.add(next);
+                    visited.add(next);
+                }
+            }
+        }
+        return null;
     }
 }
